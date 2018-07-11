502ef4980e8d84a86ad508c9ce4e8fe3f0b8508e
KimHyeockJin <gurwls9628@naver.com>
2018-07-12 01:43:21 +0900

    Converter.java: Change function to static
    Change function to static to prevent
    unnecessary object creation.

--- src/Converter.java
+++ src/Converter.java
@@ -14,34 +14,23 @@ import java.util.Optional;
 
 
 public class Converter {
-    private String filePath;
-    private String folderPath;
-    private String fileName;
-    Converter(String[] paths) throws FileNotFoundException {
-        setPath(paths[0]);
-    }
-
-    private void setPath(String paths) throws FileNotFoundException {
-        Path path = Paths.get(paths);
-        if (paths.contains("/")) {
+    private static String getFolderPath(String filePath) throws FileNotFoundException {
+        Path path = Paths.get(filePath);
+        String folderPath;
+        if (filePath.contains("/")) {
             folderPath = path.getParent().toString();
         } else {
             folderPath = Paths.get(".").toString();
         }
-        fileName = path.getFileName().toString();
-        if (!"cpp".equals(fileName.substring(fileName.length()-3, fileName.length()))) {
-            throw new FileNotFoundException();
-        }
-        filePath = path.toString();
+        return folderPath;
     }
 
-    public Optional<String> parse() {
+    public static Optional<String> parse(String filePath) {
         try {
             CPP14Lexer lexer = new CPP14Lexer(new ANTLRFileStream(filePath));
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             CPP14Parser parser = new CPP14Parser(tokens);
             ParserRuleContext tree = parser.translationunit();
-
             WeakCheckVisitor visitor = new WeakCheckVisitor(tokens);
             visitor.visit(tree);
             return Optional.ofNullable(visitor.getFullText());
@@ -51,8 +40,10 @@ public class Converter {
         return Optional.empty();
     }
 
-    public void writeCppFile(String text) {
+    public static void writeCppFile(String filePath, String text) {
         try {
+            String folderPath = getFolderPath(filePath);
+            String fileName = Paths.get(filePath).getFileName().toString();
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(Paths.get(folderPath, "_"+fileName).toString()));
             bufferedWriter.write(text);
             bufferedWriter.close();
