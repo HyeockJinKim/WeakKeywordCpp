1448eef54208ddd3e5059421f8c844fceb498cb7
Hyeock Jin <kherootz@gmail.com>
2018-08-23 21:49:20 +0900

    Main.java: No args Exception handling (#27)
    If no args, print No File input
    
    Closes https://github.com/HyeockJinKim/WeakKeywordCpp/issues/26

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String path = Paths.get(args[0]).toString();
-        ReadFile.checkDirectory(path);
+        if (args.length > 0) {
+            String path = Paths.get(args[0]).toString();
+            ReadFile.recursiveReadDirectory(path);
+        } else {
+            System.out.println("No File Input ");
+        }
     }
 }
