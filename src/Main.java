932e61e20fe8acfa09ed7aa34fd2034e4e765d89
Hyeock Jin <kherootz@gmail.com>
2018-09-12 13:46:49 +0900

    src: Allow multiple arguments (#40)
    Allow to specify the location of the base directory
     and log file by taking multiple arguments.
    
    Closes https://github.com/HyeockJinKim/WeakKeywordCpp/issues/39

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
+import processing.ReadFile;
 import java.io.IOException;
-import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        if (args.length > 0) {
-            String path = Paths.get(args[0]).toString();
-            ReadFile.recursiveReadDirectory(path);
-        } else {
-            System.out.println("No File Input ");
+    public static void main(String[] args) {
+        try {
+            ReadFile.readArgs(args);
+        } catch (IOException | IndexOutOfBoundsException e) {
+            e.printStackTrace();
         }
     }
 }
