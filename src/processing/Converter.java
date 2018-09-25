f69a9bc9fc8e6d00475cb64774ae27377bbb0bd9
KimHyeockJin <gurwls9628@naver.com>
2018-09-26 01:33:13 +0900

    Converter.java: Write Class information
    Write information about already converted files
    in order not to convert them again
    
    Closes https://github.com/HyeockJinKim/WeakKeywordCpp/issues/28, https://github.com/HyeockJinKim/WeakKeywordCpp/issues/33

--- src/processing/Converter.java
+++ src/processing/Converter.java
@@ -1,14 +1,16 @@
 package processing;
 
+import JSON.JSONArray;
+import JSON.JSONDictionary;
 import checker.ClassVisitor;
 import checker.StaticCastVisitor;
 import grammar.antlr.CPP14Lexer;
 import grammar.antlr.CPP14Parser;
 import org.antlr.v4.runtime.*;
 import weakclass.CppClass;
+import weakclass.CppFunction;
 
 import java.io.*;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Optional;
@@ -16,6 +18,8 @@ import java.util.Set;
 
 
 public class Converter {
+    static final String tag = "/* This File is Converted */";
+
     private HashSet<String> moduleSet = new HashSet<>();
     private String filePath;
     private boolean hasClass;
@@ -26,10 +30,14 @@ public class Converter {
         this.filePath = filePath;
     }
 
-    private void checkFile(String filename) {
+    private boolean checkFile(String filename) {
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
+
             while ((line = br.readLine()) != null) {
+                if (line.contains(tag)) {
+                    return false;
+                }
                 if (line.contains("#include")) {
                     if (line.contains("\"")) {
                         String temp = line.split("\"")[1];
@@ -50,7 +58,9 @@ public class Converter {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return true;
     }
+
     private Optional<String> parseClass() {
         try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
             ANTLRInputStream inputStream = new ANTLRInputStream(fileInputStream);
@@ -91,26 +101,61 @@ public class Converter {
 
     void convert() {
         classSet = new HashSet<>();
-        checkFile(filePath);
+        if (checkFile(filePath)) {
+            if (hasClass) {
+                Optional<String> result = parseClass();
+                String json = jsonifyClassSet();
+                result.ifPresent(x -> writeCppFile(x, json));
+            }
 
-        if (hasClass) {
-            Optional<String> result = parseClass();
-            result.ifPresent(this::writeCppFile);
-        }
-
-        if (hasStaticCast) {
-            Optional<String> result = parseStaticCast();
-            result.ifPresent(this::writeCppFile);
+            if (hasStaticCast) {
+                Optional<String> result = parseStaticCast();
+                result.ifPresent(x -> writeCppFile(x, tag));
+            }
         }
     }
 
-    private void writeCppFile(String file) {
+    private void writeCppFile(String file, String tag) {
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+            bw.write(tag);
+            bw.newLine();
             bw.write(file);
             System.out.println(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String jsonifyClassSet() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("/*");
+        JSONArray classesArr = new JSONArray();
+        for (CppClass cppClass : classSet) {
+            JSONDictionary classDict = new JSONDictionary();
+
+            JSONArray superSet = new JSONArray();
+            for (CppClass superClass : cppClass.superSet) {
+                superSet.add(superClass.className);
+            }
+            classDict.put("super", superSet);
+
+            JSONArray virtual = new JSONArray();
+            for (CppFunction virtualFunction : cppClass.virtualFunctionSet) {
+                JSONDictionary function = new JSONDictionary();
+                JSONArray params = new JSONArray();
+                for (String param : virtualFunction.functionParameter) {
+                    params.add(param);
+                }
+                function.put(virtualFunction.functionName, params);
+                virtual.add(function);
+            }
+            classDict.put("virtual", virtual);
+
+            classesArr.add(classDict);
+        }
+        sb.append(classesArr.toString());
+        sb.append("*/");
+        return sb.toString();
     }
 
 }
