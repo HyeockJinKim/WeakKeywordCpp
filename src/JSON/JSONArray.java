33c9a06042e2676bc015fdd96b4df8340f83d391
KimHyeockJin <gurwls9628@naver.com>
2018-09-29 23:14:32 +0900

    JSON: Make JSON parsable
    Make JSON parsable

--- src/JSON/JSONArray.java
+++ src/JSON/JSONArray.java
@@ -3,23 +3,75 @@ package JSON;
 import java.util.ArrayList;
 
 public class JSONArray extends JSONObject {
-    private ArrayList<String> jsonArray;
+    private ArrayList<JSONObject> jsonArray;
 
     public JSONArray() {
         jsonArray = new ArrayList<>();
     }
 
-    public void add(JSONDictionary json) {
-        jsonArray.add(json.toString());
-    }
-
-    public void add(JSONArray json) {
-        jsonArray.add(json.toString());
+    public void add(JSONObject json) {
+        jsonArray.add(json);
     }
 
     public void add(String value) {
-        jsonArray.add(value);
+        jsonArray.add(new JSONString(value));
     }
+
+    public int size() {
+        return jsonArray.size();
+    }
+
+    public JSONObject get(int index) {
+        return jsonArray.get(index);
+    }
+
+    static JSONArray parseJson(char[] json) {
+        JSONArray array = new JSONArray();
+        JSONObject curObject = null;
+        for (; i < json.length;) {
+            ++i;
+            switch (json[i]) {
+                case '[':
+                    curObject = JSONArray.parseJson(json);
+                    break;
+                case '{':
+                    curObject = JSONDictionary.parseJson(json);
+                    break;
+                case ',':
+                    array.add(curObject);
+                    curObject = null;
+                    break;
+                case ']':
+                    if (curObject != null)
+                        array.add(curObject);
+
+                    return array;
+                default:
+                    if (curObject == null) {
+                        curObject = new JSONString();
+                    }
+                    if (curObject instanceof JSONString)
+                        ((JSONString) curObject).add(json[i]);
+                    break;
+            }
+        }
+        System.out.println("Parsing Problem !");
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return jsonArray.size();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof JSONArray) {
+            return jsonArray.equals(((JSONArray) obj).jsonArray);
+        }
+        return false;
+    }
+
 
     @Override
     public String toString() {
@@ -27,10 +79,10 @@ public class JSONArray extends JSONObject {
         sb.append("[");
         int len = jsonArray.size();
         for (int i = 0; i < len-1; ++i) {
-            sb.append(jsonArray.get(i)).append(",");
+            sb.append(jsonArray.get(i).toString()).append(",");
         }
         if (len > 0)
-            sb.append(jsonArray.get(len-1));
+            sb.append(jsonArray.get(len-1).toString());
         sb.append("]");
         return sb.toString();
     }
--- src/JSON/JSONObject.java
+++ src/JSON/JSONObject.java
@@ -1,4 +1,17 @@
 package JSON;
 
 public class JSONObject {
+    protected static int i;
+
+    public static JSONObject parseJson(String json) {
+        char[] jsons = json.toCharArray();
+
+        for (i = 0; i < jsons.length; ++i) {
+            if (jsons[i] == '[')
+                return JSONArray.parseJson(json.toCharArray());
+            else if (jsons[i] == '{')
+                return JSONDictionary.parseJson(json.toCharArray());
+        }
+        return null;
+    }
 }
